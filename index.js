82360dfcdccfb7745859ac2723397ea8bd8dbce4
Serj Ilyashenko <serj.ilyashenko@gmail.com>
2017-06-01 22:36:55 +0300

    semantic-ui dependency

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-pagination-addon'
+  name: 'ember-pagination-addon',
+
+  afterInstall: function() {
+    // Add addons to package.json and run defaultBlueprint
+    return this.addAddonsToProject({
+      // a packages array defines the addons to install
+      packages: [
+        // name is the addon name, and target (optional) is the version
+        {name: 'semantic-ui-ember'},
+        // {name: 'ember-cli-sass'}
+      ]
+    });
+  },
 };
